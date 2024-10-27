73974e19bbb3127220227ab4282ac65bb2a3078e
dev-hafiz <johnstarhafiz@gmail.com>
2024-10-27 11:42:36 +0600

    An array in TypeScript is a collection of elements, each identified by an index. It's a versatile data structure that allows you to store and manipulate multiple values under a single variable name.

---
+++ dist/array.js
@@ -0,0 +1,6 @@
+"use strict";
+//* Basic Declaration:
+let numbers = [1, 2, 3, 4, 5];
+let fruits = ["apple", "banana", "cherry"];
+//* Type Inference:
+let inferredNumbers = [1, 2, 3]; // Inferred type: number[]
---
+++ src/array.ts
@@ -0,0 +1,8 @@
+//* Basic Declaration:
+
+let numbers: number[] = [1, 2, 3, 4, 5];
+let fruits: string[] = ["apple", "banana", "cherry"];
+
+//* Type Inference:
+let inferredNumbers = [1, 2, 3]; // Inferred type: number[]
+

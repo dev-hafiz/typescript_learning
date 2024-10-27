038972d6a9bffa6a689707e968a1890acaf449cb
dev-hafiz <johnstarhafiz@gmail.com>
2024-10-27 10:20:09 +0600

    Primitive Types in Typescript

---
+++ dist/types.js
@@ -0,0 +1,9 @@
+"use strict";
+// Primitive Types
+let num = 42;
+let str = "Hello, TypeScript!";
+let bool = true;
+let nul = null;
+let und = undefined;
+let sym = Symbol("unique");
+let big = 1234567890123456789n;
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+// Primitive Types
+let num: number = 42;
+let str: string = "Hello, TypeScript!";
+let bool: boolean = true;
+let nul: null = null;
+let und: undefined = undefined;
+let sym: symbol = Symbol("unique");
+let big: bigint = 1234567890123456789n;

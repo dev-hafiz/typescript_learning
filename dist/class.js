7f46277a7fc021331ae6b804c7c62f9978a2e0c4
dev-hafiz <johnstarhafiz@gmail.com>
2024-10-28 09:26:11 +0600

    Getter and setter in ts

--- dist/class.js
+++ dist/class.js
@@ -1,6 +1,6 @@
 "use strict";
-// class Teacher {
-//     name : string
+//class Teacher {
+//    name : string
 //     email : string
 //     readonly city : string = "New York"
 //     constructor(name : string, email: string){
@@ -17,6 +17,20 @@ class Teacher {
         this.teacherId = teacherId;
         this.subject = subject;
         this.city = city;
+        this._courseCount = 1;
+    }
+    get getAppleEmail() {
+        return `Apple ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const alice = new Teacher("Alice", "alice@touch.com", 23423424, "English", "New York");
+console.log(alice.courseCount);
--- src/class.ts
+++ src/class.ts
@@ -1,6 +1,6 @@
 
-// class Teacher {
-//     name : string
+//class Teacher {
+//    name : string
 //     email : string
 //     readonly city : string = "New York"
 //     constructor(name : string, email: string){
@@ -16,16 +16,31 @@
 
 class Teacher {
    
+    private _courseCount : number = 1
     constructor(
-        public name : string, 
+       public name : string, 
        private email: string, 
        private teacherId: number, 
        public subject : string, 
        readonly city: string
     ){
-        
+    }
+
+    get getAppleEmail() : string {
+        return `Apple ${this.email}`
+    }
+
+    get courseCount() : number{
+        return this._courseCount
+    }
+
+    set courseCount(courseNum){
+        if(courseNum <= 1){
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
     }
 }
 
 const alice = new Teacher("Alice", "alice@touch.com", 23423424, "English", "New York")
-
+console.log(alice.courseCount)
